acd4e947e3b37fb7c8beae5d5aabac57ab10b806
Jesse Koldewijn <jesse@iungo.nl>
2023-04-20 15:54:52 +0200

    init: codebase creation

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center">Welcome to ShiftCoder 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000" />
+  <a href="https://#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> A simple but high performant c++ based application build with GLFW.
+
+## Prerequisites
+- make + cmake
+- C-compiler / Visual Studio c++ buildtools
+---
+## Usage
+
+1. `mkdir build`
+2. `cd build`
+3. `cmake ..` (for MinGW the build system generator needs to be specified using the -G option: `cmake .. -G "MinGW Makefiles"`)
+4. Run `make all`
+---
+## Author
+👤 **ShiftCode Developments**
+* Website: https://shiftcode.eu
+* Github: [@ShiftCodeEU](https://github.com/ShiftCodeEU)
+---
+## Show your support
+Give a ⭐️ if this project helped you!
+
+---
+## 📝 License
+Copyright © 2023 [ShiftCode Developments](https://github.com/ShiftCodeEU).<br />
+This project is [MIT](https://#) licensed.
+
+---
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <windows.h>
+#include <GLFW/glfw3.h>
+
+// change this to int main() to allow the console
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char *, int nShowCmd)
+{
+    GLFWwindow *window;
+    int windowSizeW = 640, windowSizeH = 480;
+
+    // shared_ptr<GLFont> glFont = shared_ptr<GLFont>(new GLFont(".../myFont.ttf"));
+
+    // initialize the library
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+    int count, windowWidth, windowHeight, monitorX, monitorY;
+
+    // I am assuming that main monitor is in the 0 position
+    GLFWmonitor **monitors = glfwGetMonitors(&count);
+    const GLFWvidmode *videoMode = glfwGetVideoMode(monitors[0]);
+    // width: 75% of the screen
+    windowWidth = static_cast<int>(videoMode->width / 1.5);
+    // aspect ratio 16 to 9
+    windowHeight = static_cast<int>(videoMode->height / 16 * 9);
+
+    glfwGetMonitorPos(monitors[0], &monitorX, &monitorY);
+
+    // set the visibility window hint to false for subsequent window creation
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+
+    // create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(windowSizeW, windowSizeH, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // reset the window hints to default
+    glfwDefaultWindowHints();
+
+    glfwSetWindowPos(window,
+                     monitorX + (videoMode->width - windowWidth) / 2,
+                     monitorY + (videoMode->height - windowHeight) / 2);
+
+    // show the window
+    glfwShowWindow(window);
+
+    // uncomment following line to see the border of window
+    glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
+
+    float fAngle = 0.0f;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+        glBegin(GL_QUADS);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
